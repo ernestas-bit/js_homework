6148080cf91299f80f5ad093dc4d3f43f0826e9b
ernestas-bit <bit.ernestas@gmail.com>
2022-05-16 14:14:48 +0300

    Changed function homework

--- 4_functions/5/index.js
+++ 4_functions/5/index.js
@@ -1,8 +1,13 @@
 const listOfNames = [ 'lukas', 'tomas', 'borisas', 'liepa', 'simas' ];
 /* UŽDUOTIS
 DUOTA: listOfNames string'ų masyvas
-sukurkite funkciją capitalize, kuri priims šį masyvą kaip parametrą ir gražins 
-naują masyvą, kur visos pirmos raidės bus didžiosios
+1) Sukurkite funkciją capitalize - ši funkcija turi priimti stringą kaip parametrą
+ir gražinti šį Stringą taip, kad jo pirmoji raidė visada būtų didžioji.
+2) Sukurkite funkciją capitalizeArray - ši funkcija kaip parametrą priims string'ų
+masyvą. 
+2.1) Šios funkcijos viduje naudojant ciklą pakeiskite visus narius naudodami pirmoje
+dalyje aprašytą funkciją, sudėkite juos į naują masyvą ir jį gražinkite
+
 (Patarimai: naudoti string slice metodą)
 */
 
