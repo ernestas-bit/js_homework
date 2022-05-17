b8d18e5186f083cd2ca38a6daa402d48d1e954a8
ernestas-bit <bit.ernestas@gmail.com>
2022-05-17 14:17:40 +0300

    Added more functions homework

---
+++ 5_more_functions/1/index.js
@@ -0,0 +1,11 @@
+/* UŽDUOTIS: 
+1.1) Sukurkite funkciją, kuri priims vieną parametrą - skaičių ir gražins tą skaičių
+pakeltą kvadratu. Šią funkcija išsaugokite kintamąjame pavadinimu square.
+
+square(2) // 4
+square(5) // 25
+*/
+
+// KODAS PRASIDEDA ČIA
+
+// BAIGIASI ČIA
---
+++ 5_more_functions/2/index.js
@@ -0,0 +1,14 @@
+/* UŽDUOTIS - Sukurkite metodus chicken objektui
+1) Sukurkite metodą greet, kuris išspausdins į konsolę 
+("Hello, my name is (...name...)")
+2) Sukurkite metodą getAge, kuris išspaudins kiek metų vištai
+3) Sukurkite metodą layEgg, kuris padidins eggCount vienu ir išspausdins į
+konsolę "LAYING EGG", "NEW EGG COUNT: (...eggCount...)"  
+
+*/
+
+const chicken = {
+	name: 'Helen',
+	eggCount: 0,
+	born_in: 2004
+}
