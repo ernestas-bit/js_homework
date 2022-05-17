934e7e6667f4bf44b9194cb89a051fb335959acf
ernestas-bit <bit.ernestas@gmail.com>
2022-05-17 14:31:15 +0300

    Added another homework for methods

---
+++ 5_more_functions/3/index.js
@@ -0,0 +1,15 @@
+/* UŽDUOTIS - sukurkite metodus car objektui
+1) Sukurkite metodą addOwner, kuris priims parametrą ir į owners masyvą 
+pridės naują sąvininką
+2) Sukurkite metodą getOwner, kuris priims kaip parametrą skaičių (1 ir daugiau)
+ir išspausdins į konsolę "Adding owner (...owner...)"(1 - atitiks owners[0])
+3) Sukurkite metodą deleteOwner, kuris priims skaičių kaip parametrą ir ištrins
+sąvininką, ir  išspausdins į konsolę ("Deleting owner (...owner...)")
+
+*/
+
+const car = {
+	model: 'Audi',
+	year: 2002,
+	owners: [ 'Petras', 'Antanas', 'Kazys' ]
+}
