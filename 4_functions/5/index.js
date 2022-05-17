465fa9650458db5089359db81e10d05d974aba5c
ernestas-bit <bit.ernestas@gmail.com>
2022-05-17 09:05:37 +0300

    formatting

--- 4_functions/5/index.js
+++ 4_functions/5/index.js
@@ -1,14 +1,16 @@
-const listOfNames = [ 'lukas', 'tomas', 'borisas', 'liepa', 'simas' ];
+const listOfNames = [ 'lukas', 'tomas', 'borisas', 'liepa', 'simas' ]
 /* UŽDUOTIS
 DUOTA: listOfNames string'ų masyvas
-2) Sukurkite funkciją capitalizeArray - ši funkcija kaip parametrą priims string'ų
+1) Sukurkite funkciją capitalizeArray - ši funkcija kaip parametrą priims string'ų
 masyvą. 
-2.1) Šios funkcijos viduje naudojant ciklą pakeiskite visus narius taip, kad pirmoji
+1.1) Šios funkcijos viduje naudojant ciklą pakeiskite visus narius taip, kad pirmoji
 raidė visada būtų didžioji. Juos sudėkite į naują masyvą ir grąžinkite funkcijoje
-
+Naudodami console.log() pasitikrinkite atsakymą
 (Patarimai: naudoti string slice metodą)
 */
 
 // KODAS PRASIDEDA ČIA
+
 function capitalizeArray() {}
+
 // BAIGIASI ČIA
