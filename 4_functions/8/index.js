bd1d275e1ab39a90705fdd57d78911d4c0966384
ernestas-bit <bit.ernestas@gmail.com>
2022-05-17 09:01:33 +0300

    Added another function homework

---
+++ 4_functions/8/index.js
@@ -0,0 +1,27 @@
+const birthYears = {
+	jonas: 1994,
+	lina: 2001,
+	petras: 1990,
+	kristina: 1960,
+	paulius: 1988,
+	julius: 2011,
+	janina: 1940
+};
+
+/* UŽDUOTIS - apskaičiuoti objekte esančių narių amžių
+1) Baikite rašyti birthYear funkciją - ji turės priimti parametrą - objektą žmonių
+amžių. 
+2) Funkcija turėtų sukurti ir grąžinti naują objektą, kuriame matytųsi tikras
+narių amžius
+{ jonas: 26,
+  lina: 21
+... };
+
+
+*/
+
+// KODAS PRASIDEDA ČIA
+
+function birthYears() {}
+
+// BAIGIASI ČIA
