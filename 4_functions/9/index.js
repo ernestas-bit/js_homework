202a91d2562fd45aa29cbd82a1f68803f128599e
ernestas-bit <bit.ernestas@gmail.com>
2022-05-22 09:50:54 +0300

    Added extra loop homework

---
+++ 4_functions/9/index.js
@@ -0,0 +1,13 @@
+/* UŽDUOTIS - pabaikite rašyti funkciją fizzBuzz
+1) Ši funkcija turėtų sukti ciklą nuo 1 iki 100 (įskaitant)
+2) Jei ciklo indeksas dalinasi iš 3 be liekanos išspausdinkite konsolėje Fizz
+3) Jei ciklo indeksas dalinasi iš 5 be liekanos konsolėje išspausdinkite Buzz
+4) Jei dalinasi ir iš 3 ir iš 5 - išspausdinkite FizzBuzz
+5) Kitu atveju tiesiog išspausdinkite indeksą
+
+ 
+
+*/
+
+function fizzBuzz() {}
+fizzBuzz()
