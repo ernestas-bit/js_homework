f7285d6904cee7a63da284505e5c5e9c4b26e156
ernestas-bit <bit.ernestas@gmail.com>
2022-05-22 10:02:51 +0300

    Added wiki hint

--- 4_functions/6/index.js
+++ 4_functions/6/index.js
@@ -1,8 +1,8 @@
-const btn = document.querySelector('#evaluate_triangle');
-const answer = document.querySelector('#answer');
-const x = document.querySelector('#side1');
-const y = document.querySelector('#side2');
-const z = document.querySelector('#side3');
+const btn = document.querySelector('#evaluate_triangle')
+const answer = document.querySelector('#answer')
+const x = document.querySelector('#side1')
+const y = document.querySelector('#side2')
+const z = document.querySelector('#side3')
 
 /*  UŽDUOTIS - Baigite rašyti triangle funkciją
 1) Funkciją turi priimti tris kraštines kaip parametrus
@@ -13,16 +13,17 @@ const z = document.querySelector('#side3');
 
 Gerai atlikus užduotiį HTML'e suvedę kraštines ir paspaudę "Koks trikampis"
 matysite rezultatą
+Matematiką susiraskite wikipedijoje
 */
 
 // KODAS PRASIDEDA ČIA
 
 function triangle() {
-	return 'Tai ne trikampis';
+	return 'Tai ne trikampis'
 }
 
 // BAIGIASI ČIA
 
 btn.addEventListener('click', () => {
-	answer.innerText = triangle(parseInt(x.value), parseInt(y.value), parseInt(z.value));
-});
+	answer.innerText = triangle(parseInt(x.value), parseInt(y.value), parseInt(z.value))
+})
