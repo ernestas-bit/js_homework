0cc68b563a5e7b864f494545f09e15b953d57e55
ernestas-bit <bit.ernestas@gmail.com>
2022-05-23 18:08:53 +0300

    updated

---
+++ 7_dom_events/3/index.js
@@ -0,0 +1,10 @@
+/* UŽDUOTIS - Darbas su forma JavaScript'e 
+1) Sustabdykite formos defaultinį submit elgesį 
+2) Iš įvesties sukurkite li elementą ir į jį įdėkite tekstą: 
+Prekė: (...prekė...), Kiekis: (...kiekis...) 
+3) Įdėkite li elementą į listą su id groceries(jau yra HTML'e)
+4) Jei neįvesta prekė arba kiekis - į <p> (jau yra HTML'e) su id error įrašykite: 
+"Oops, pamiršote įvesti... (...Ko neįvedė...)"
+5) Paspaudus submit, jei įvestis sėkminga reikėtų išvalyti input'us ir error 
+žinutę jei tokia yra
+*/
